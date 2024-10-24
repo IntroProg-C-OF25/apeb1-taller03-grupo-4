8f52be8201a639c568b27f6f35c8e599598c285d
MatthewFlores2006 <cuentaalternativamatthewflores@gmail.com>
2024-10-24 12:17:41 -0500

    Segundo commit

--- ProyectoTaller3/src/Ejercicio1.java
+++ ProyectoTaller3/src/Ejercicio1.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Lenovo
- */
 public class Ejercicio1 {
-    
+    public static void main(String[] args) {
+        double respuesta;
+        respuesta = ( (1 + 2 + 4 + 3) / 5 ) + Math.sqrt(81);
+        System.out.println("respuesta = " + respuesta);
+    }
 }
